bb0ed472631a7097cdc76eadccc47a9408d303ce
milesillsley <milesillsley@hotmail.co.uk>
2017-04-26 14:51:51 +0100

    Adding Play view

--- app.rb
+++ app.rb
@@ -2,12 +2,16 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  # get '/' do
-  #   'Testing infrastructure working!'
-  # end
+  enable :session
 
   get '/' do
     erb :index
+  end
+
+  post '/names' do
+    @P1 = params[:P1]
+    @P2 = params[:P2]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/name_input_form_spec.rb
+++ spec/features/name_input_form_spec.rb
@@ -1,8 +1,8 @@
 feature 'Input names in form' do
   scenario 'user can input name in form' do
     visit('/')
-    fill_in('Player 1: ', with: 'Jess')
-    fill_in('Player 2: ', with: 'Miles')
+    fill_in('P1', with: 'Jess')
+    fill_in('P2', with: 'Miles')
     click_button('Submit')
     expect(page).to have_content 'Player 1: Jess Player 2: Miles'
   end
